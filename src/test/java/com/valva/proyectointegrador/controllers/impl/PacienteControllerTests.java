e3272631a1a83a3e6c1427866c570bd6715b87ba
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2021-09-30 00:36:41 -0300

    fix PacienteControllerTests in order to make them independent and isolated

--- src/test/java/com/valva/proyectointegrador/controllers/impl/PacienteControllerTests.java
+++ src/test/java/com/valva/proyectointegrador/controllers/impl/PacienteControllerTests.java
@@ -1,46 +1,53 @@
 package com.valva.proyectointegrador.controllers.impl;
 
+import com.valva.proyectointegrador.exceptions.GlobalExceptionsHandler;
 import com.valva.proyectointegrador.model.DomicilioDto;
 import com.valva.proyectointegrador.model.PacienteDto;
 import com.valva.proyectointegrador.utils.Mapper;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-@RunWith(SpringRunner.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(addFilters = false)
+@WebAppConfiguration
 public class PacienteControllerTests {
 
     @Autowired
+    private PacienteController pacienteController;
+
     private MockMvc mockMvc;
-    @Test
-    public void test01registrarPacientes() throws Exception {
+
+    @Before
+    public void reset() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(pacienteController).setControllerAdvice(GlobalExceptionsHandler.class).build();
         PacienteDto p1 = new PacienteDto("Pepe", "Pepardo", 111111111, new DomicilioDto("Calle", 123, "Caballito", "CABA"));
         PacienteDto p2 = new PacienteDto("Pepa", "Peparda", 111111110, new DomicilioDto("Lleca", 321, "Caballito", "CABA"));
         mockMvc.perform(MockMvcRequestBuilders.post("/pacientes").contentType(MediaType.APPLICATION_JSON).content(Mapper.mapObjectToJson(p1)));
         mockMvc.perform(MockMvcRequestBuilders.post("/pacientes").contentType(MediaType.APPLICATION_JSON).content(Mapper.mapObjectToJson(p2)));
+    }
 
+    @Test
+    public void test01registrarPacientes() throws Exception {
         PacienteDto p = new PacienteDto("Pepe", "Pepin", 123456789, new DomicilioDto("Calle", 123, "Caballito", "CABA"));
         MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post("/pacientes")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -120,7 +127,7 @@ public class PacienteControllerTests {
 
     @Test
     public void test08ActualizarPacienteInexistenteDevuelveNotFound() throws Exception {
-        PacienteDto p = new PacienteDto("Pepe", "Pepin", 123456789, new DomicilioDto("Calle", 123, "Caballito", "CABA"));
+        PacienteDto p = new PacienteDto("Pepe", "Pepardo", 111111111, new DomicilioDto("Calle", 123, "Caballito", "CABA"));
         p.setId(99);
         mockMvc.perform(MockMvcRequestBuilders
                         .put("/pacientes")
@@ -133,9 +140,9 @@ public class PacienteControllerTests {
 
     @Test
     public void test09ActualizarPacienteExistenteDevuelveOk() throws Exception {
-        DomicilioDto d =  new DomicilioDto("Calle", 123, "Caballito", "CABA");
+        DomicilioDto d =  new DomicilioDto("Calle", 111, "Caballito", "CABA");
         d.setId(1);
-        PacienteDto p = new PacienteDto("Pepe", "Pepiiiiin", 123456789, d);
+        PacienteDto p = new PacienteDto("Pepe", "Pepardisimo", 111111111, d);
         p.setId(1);
         p.setFechaIngreso(LocalDate.now());
         MvcResult response = mockMvc.perform(MockMvcRequestBuilders
@@ -159,7 +166,7 @@ public class PacienteControllerTests {
     }
 
     @Test
-    public void test11eliminarPacientePorIdExistenteDevuelveOk() throws Exception {
+    public void test00eliminarPacientePorIdExistenteDevuelveOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/pacientes/{id}", "2"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
